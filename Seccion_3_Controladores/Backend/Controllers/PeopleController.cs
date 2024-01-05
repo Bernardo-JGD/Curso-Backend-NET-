007d7744b571e2cfdd93b177df332d90d47813d8
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2024-01-05 15:20:49 -0600

    Sección 5 Terminada
    Controladores, Respuestas HTTP e Inyección de dependencias (Singleton, Scoped, Transient)

---
+++ Seccion_3_Controladores/Backend/Controllers/PeopleController.cs
@@ -0,0 +1,104 @@
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private IPeopleService _peopleService;
+
+        public PeopleController( [FromKeyedServices("peopleService2")] IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+
+        [HttpGet("{id}")]
+        public ActionResult<People> Get(int id) { 
+            var people = Repository.People.FirstOrDefault(p => p.Id == id);
+            if (people == null)
+            {
+                return NotFound();
+            }
+            return Ok(people);
+        }
+
+        [HttpPost]
+        public IActionResult Add(People people)
+        {
+            if (!_peopleService.Validate(people))
+            {
+                return BadRequest();
+            }
+
+            Repository.People.Add(people);
+            return NoContent();
+        }
+
+        [HttpGet("search/{search}")]
+        public List<People> Get(string search) =>
+            Repository.People.Where(p => p.Name.Contains(search)).ToList();
+
+        [HttpGet("searchopen/{search}")]
+        public List<People> GetOpen(string search) =>
+            Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+
+
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Panda Rojo",
+                Birthdate = new DateTime(1999, 10, 01)
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Panda Azul",
+                Birthdate = new DateTime(1998, 09, 02)
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Panda Verde",
+                Birthdate = new DateTime(1997, 08, 03)
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Panda Amarillo",
+                Birthdate = new DateTime(1999, 10, 01)
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Panda Morado",
+                Birthdate = new DateTime(1998, 09, 02)
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Panda Cafe",
+                Birthdate = new DateTime(1997, 08, 03)
+            },
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+}
---
+++ Seccion_3_Controladores/Backend/Controllers/RandomController.cs
@@ -0,0 +1,53 @@
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private IRandomService _randomServiceSingleton;
+        private IRandomService _randomServiceScoped;
+        private IRandomService _randomServiceTransient;
+
+        private IRandomService _randomServiceSingleton2;
+        private IRandomService _randomServiceScoped2;
+        private IRandomService _randomServiceTransient2;
+
+        public RandomController(
+            [FromKeyedServices("randomSingleton")] IRandomService randomServiceSingleton,
+            [FromKeyedServices("randomScoped")] IRandomService randomServiceScoped,
+            [FromKeyedServices("randomTransient")] IRandomService randomServiceTransient,
+            [FromKeyedServices("randomSingleton")] IRandomService randomServiceSingleton2,
+            [FromKeyedServices("randomScoped")] IRandomService randomServiceScoped2,
+            [FromKeyedServices("randomTransient")] IRandomService randomServiceTransient2
+        )
+        {
+            _randomServiceSingleton = randomServiceSingleton;
+            _randomServiceScoped = randomServiceScoped;
+            _randomServiceTransient = randomServiceTransient;
+            _randomServiceSingleton2 = randomServiceSingleton2;
+            _randomServiceScoped2 = randomServiceScoped2;
+            _randomServiceTransient2 = randomServiceTransient2;
+        }
+
+        [HttpGet]
+        public ActionResult<Dictionary<string, int>> Get()
+        {
+            var result = new Dictionary<string, int>();
+            result.Add("Singleton 1", _randomServiceSingleton.Value);
+            result.Add("Scoped 1", _randomServiceScoped.Value);
+            result.Add("Transient 1", _randomServiceTransient.Value);
+
+            result.Add("Singleton 2", _randomServiceSingleton2.Value);
+            result.Add("Scoped 2", _randomServiceScoped2.Value);
+            result.Add("Transient 2", _randomServiceTransient2.Value);
+
+            return result;
+
+        }
+
+    }
+}
