8862f2561a3b33c377cdab00a39a2300b9e850f2
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2024-01-27 10:43:56 -0600

    Sección 13 terminada
    Automapper

--- Seccion_3_Controladores/Backend/Program.cs
+++ Seccion_3_Controladores/Backend/Program.cs
@@ -1,3 +1,4 @@
+using Backend.Automappers;
 using Backend.DTOs;
 using Backend.Models;
 using Backend.Repository;
@@ -41,6 +42,8 @@ builder.Services.AddDbContext<StoreContext>(options =>
 builder.Services.AddScoped<IValidator<BeerInsertDto>, BeerInsertValidator>();
 builder.Services.AddScoped<IValidator<BeerUpdateDto>, BeerUpdateValidator>();
 
+//Automapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Seccion_3_Controladores/Backend/Services/BeerService.cs
+++ Seccion_3_Controladores/Backend/Services/BeerService.cs
@@ -1,4 +1,5 @@
-﻿using Backend.DTOs;
+﻿using AutoMapper;
+using Backend.DTOs;
 using Backend.Models;
 using Backend.Repository;
 using Microsoft.EntityFrameworkCore;
@@ -8,16 +9,23 @@ namespace Backend.Services
     public class BeerService : ICommonService<BeerDto, BeerInsertDto, BeerUpdateDto>
     {
         private IRepository<Beer> _beerRepository;
+        private IMapper _mapper;
 
-        public BeerService(IRepository<Beer> beerRepository)
+        public BeerService(
+            IRepository<Beer> beerRepository,
+            IMapper mapper
+        )
         {
             _beerRepository = beerRepository;
+            _mapper = mapper;
         }
 
         public async Task<IEnumerable<BeerDto>> Get()
         {
             var beers = await _beerRepository.Get();
 
+            //En vez de hacerlo así...
+            /*
             return beers.Select(b => new BeerDto
             {
                 Id = b.BeerID,
@@ -25,6 +33,9 @@ namespace Backend.Services
                 Alcohol = b.Alcohol,
                 BrandID = b.BrandID
             });
+            */
+            //Ahora usaremos mapper
+            return beers.Select(b => _mapper.Map<BeerDto>(b));
         }
 
         public async Task<BeerDto> GetById(int id)
@@ -33,6 +44,8 @@ namespace Backend.Services
 
             if (beer != null)
             {
+                //En vez de hacerlo así...
+                /*
                 var beerDto = new BeerDto
                 {
                     Id = beer.BeerID,
@@ -40,6 +53,10 @@ namespace Backend.Services
                     Alcohol = beer.Alcohol,
                     BrandID = beer.BrandID
                 };
+                */
+                //... ahora usaremos mapper
+                var beerDto = _mapper.Map<BeerDto>(beer);
+
                 return beerDto;
             }
 
@@ -48,12 +65,18 @@ namespace Backend.Services
 
         public async Task<BeerDto> Add(BeerInsertDto beerInsertDto)
         {
+
+            /*
             var beer = new Beer()
             {
                 Name = beerInsertDto.Name,
                 BrandID = beerInsertDto.BrandID,
                 Alcohol = beerInsertDto.Alcohol
             };
+            */
+            //En vez de hacer lo anterior manualmente,
+            //Ahora lo hace automaticamente mapper
+            var beer = _mapper.Map<Beer>(beerInsertDto);
 
             //Aquí todavía no se hace el insert en la debe
             //solo le avisa a entityFramework que va a  haber un insert
@@ -61,6 +84,7 @@ namespace Backend.Services
             //Aquí ya se ven reflejados los cambios en la base de datos
             await _beerRepository.Save();
 
+            /*
             var beerDto = new BeerDto
             {
                 Id = beer.BeerID,
@@ -68,6 +92,8 @@ namespace Backend.Services
                 Alcohol = beer.Alcohol,
                 BrandID = beer.BrandID
             };
+            */
+            var beerDto = _mapper.Map<BeerDto>(beer);
 
             return beerDto;
         }
@@ -78,15 +104,22 @@ namespace Backend.Services
 
             if (beer != null)
             {
+                //En vez de hacer esto así...
+                /*
                 beer.Name = beerUpdateDto.Name;
                 beer.Alcohol = beerUpdateDto.Alcohol;
                 beer.BrandID = beerUpdateDto.BrandID;
+                */
+                //... lo haremos asi. Y respeta el Id/BeerID
+                beer = _mapper.Map<BeerUpdateDto, Beer>(beerUpdateDto, beer);
 
                 //aquí no tiene await porque solo está haciendo un Atach
                 //de la entidad beer, 
                 _beerRepository.Update(beer);
                 await _beerRepository.Save();
 
+                //En vez de hacerlo así...
+                /*
                 var beerDto = new BeerDto
                 {
                     Id = beer.BeerID,
@@ -94,6 +127,10 @@ namespace Backend.Services
                     Alcohol = beer.Alcohol,
                     BrandID = beer.BrandID
                 };
+                */
+
+                //... ahora usaremos mapper
+                var beerDto = _mapper.Map<BeerDto>(beer);
 
                 return beerDto;
             }
@@ -108,6 +145,8 @@ namespace Backend.Services
             if (beer != null)
             {
                 //guardo primero el dto porque se va a eliminar y ya no lo recuperaría
+                //Y en vez de hacerlo asi...
+                /*
                 var beerDto = new BeerDto
                 {
                     Id = beer.BeerID,
@@ -115,6 +154,9 @@ namespace Backend.Services
                     Alcohol = beer.Alcohol,
                     BrandID = beer.BrandID
                 };
+                */
+                //... ahora lo haremos así con mapper
+                var beerDto = _mapper.Map<BeerDto>(beer);
 
                 _beerRepository.Delete(beer);
                 await _beerRepository.Save();
